dd3f8192559f0bfe8f5077c4ac0c352d37941f28
Isnotalwi <hilosarry7@gmail.com>
2025-01-14 21:28:15 +0700

    Create Main loader Honey Valley.lua

---
+++ Main loader Honey Valley.lua
@@ -0,0 +1,11 @@
+if _G.Honey_Valley then
+  warn("Script Is fucking Already running",0) 
+    return
+end
+
+_G.Honey_Valley = true
+local queueteleport = queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
+if queueteleport then
+    queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/Isnotalwi/Tower-defense-simulator/refs/heads/main/Honey%20Valley.lua'))()")
+end
+
